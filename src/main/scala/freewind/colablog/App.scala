84c15e6c6b7a9fd8f8fa0bf90b44ac92fe290e02
Freewind <pli@thoughtworks.com>
2014-05-08 14:45:32 +0800

    Add a webview to preview the html converted from markdown

--- src/main/scala/freewind/colablog/App.scala
+++ src/main/scala/freewind/colablog/App.scala
@@ -2,19 +2,48 @@ package freewind.colablog
 
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.scene.layout.{Priority, HBox}
+import scalafx.scene.layout.{Pane, Priority, HBox}
 import scalafx.scene.control.TextArea
 import org.markdown4j.Markdown4jProcessor
+import scalafx.scene.web.WebView
+import javafx.beans.value.{ObservableValue, ChangeListener}
+
 
 object App extends JFXApp {
 
-  val txtEditor = new TextArea {
+  val WidthUnit = 1
+
+  val editor = new TextArea {
+    prefWidth = WidthUnit
     hgrow = Priority.ALWAYS
-    text = "markdown here"
+    text = """## markdown here ##
+             |
+             |> Hello ColaBlog!""".stripMargin
   }
-  val txtPreview = new TextArea {
+
+  val preview = new WebView {
+    prefWidth = WidthUnit
     hgrow = Priority.ALWAYS
-    text <== txtEditor.text.map(markdown2html)
+    engine.loadContent(markdown2html(editor.text.value))
+
+    editor.text.addListener(new ChangeListener[String] {
+      override def changed(observable: ObservableValue[_ <: String], oldValue: String, newValue: String) {
+        engine.loadContent(markdown2html(newValue))
+      }
+    })
+  }
+
+  val theScene: Scene = new Scene {
+    theScene =>
+    content = new HBox {
+      prefWidth <== theScene.width
+      prefHeight <== theScene.height
+      hgrow = Priority.ALWAYS
+      content = List(
+        editor,
+        preview
+      )
+    }
   }
 
   stage = {
@@ -22,18 +51,7 @@ object App extends JFXApp {
       title = "Cola Blog"
       width = 1200
       height = 600
-      scene = new Scene {
-        theScene =>
-        content = new HBox {
-          prefWidth <== theScene.width
-          prefHeight <== theScene.height
-          hgrow = Priority.ALWAYS
-          content = List(
-            txtEditor,
-            txtPreview
-          )
-        }
-      }
+      scene = theScene
     }
   }
 
